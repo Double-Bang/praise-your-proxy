284f68cfb199384de807199b34e186b476d661c3
molloy <yo@molloy.soy>
2023-05-16 14:09:33 -0500

    Add conditional rendering to navbar component
    This adds the session object to the navbar, so that we can conditionally
    render the signin button when there is no session, and render everything
    else when there is one.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,74 +1,77 @@
 import { ReactNode } from "react";
+import { useSession, signIn, signOut } from "next-auth/react"
 import {
-  Box,
-  Flex,
-  Avatar,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useColorModeValue,
-  Stack,
-  useColorMode,
-  Center,
+    Box,
+    Flex,
+    Avatar,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useColorModeValue,
+    Stack,
+    useColorMode,
+    Center,
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon, AddIcon } from "@chakra-ui/icons";
 
 export default function Navbar() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>Praise Your Proxy</Box>
+    const {data: session } = useSession();
+    const { colorMode, toggleColorMode } = useColorMode();
+    return (
+        <>
+            <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+                <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+                    <Box>Praise Your Proxy</Box>
+                    <Flex alignItems={"center"}>
+                        {session ? 
+                         <Stack direction={"row"} spacing={7}>
+                             <Button
+                                 variant={'solid'}
+                                 size="sm"
+                                 colorScheme={'blue'}
+                                 leftIcon={<AddIcon />}>
+                                 Post a Review
+                             </Button>
 
-          <Flex alignItems={"center"}>
-            <Stack direction={"row"} spacing={7}>
-            <Button
-              variant={'solid'}
-              size="sm"
-              colorScheme={'blue'}
-              leftIcon={<AddIcon />}>
-              Post a Review
-            </Button>
+                             <Button 
+                                 size="sm"
+                                 onClick={toggleColorMode}>
+                                 {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                             </Button>
 
-              <Button 
-              size="sm"
-              onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-              </Button>
-
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rounded={"full"}
-                  variant={"link"}
-                  cursor={"pointer"}
-                  minW={0}
-                >
-                  <Avatar size={"sm"} src={""} />
-                </MenuButton>
-                <MenuList alignItems={"center"}>
-                  <br />
-                  <Center>
-                    <Avatar size={"2xl"} src={""} />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>Username</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>Your Reviews</MenuItem>
-                  <MenuItem>Logout</MenuItem>
-                </MenuList>
-              </Menu>
-            </Stack>
-          </Flex>
-        </Flex>
-      </Box>
-    </>
-  );
+                             <Menu>
+                                 <MenuButton
+                                     as={Button}
+                                     rounded={"full"}
+                                     variant={"link"}
+                                     cursor={"pointer"}
+                                     minW={0}
+                                 >
+                                     <Avatar size={"sm"} src={""} />
+                                 </MenuButton>
+                                 <MenuList alignItems={"center"}>
+                                     <br />
+                                     <Center>
+                                         <Avatar size={"2xl"} src={""} />
+                                     </Center>
+                                     <br />
+                                     <Center>
+                                         <p>Username</p>
+                                     </Center>
+                                     <br />
+                                     <MenuDivider />
+                                     <MenuItem>Your Reviews</MenuItem>
+                                     <MenuItem>Logout</MenuItem>
+                                 </MenuList>
+                             </Menu>
+                         </Stack>
+                        : <Button onClick={signIn()}>Sign in</Button>}
+                    </Flex>
+                </Flex>
+            </Box>
+        </>
+    );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import { SessionProvider } from "next-auth/react";
 export default function App({Component, pageProps:{session, ...pageProps}}){
     return(
         <SessionProvider session={session}>
-        <ChakraProvider>
-            <Component {...pageProps}/>
-        </ChakraProvider>
+            <ChakraProvider>
+                <Component {...pageProps}/>
+            </ChakraProvider>
         </SessionProvider>
     )
 }
