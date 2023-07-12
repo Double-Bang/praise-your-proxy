128be3103667bfca5416203e0aaacdfd3c075920
molloy <yo@molloy.soy>
2023-07-12 11:14:19 -0500

    Fix process.env not working
    [0] https://stackoverflow.com/questions/68205264/environment-variables-in-nextjs-are-undefined

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    clientId: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+  }
 }
 
 module.exports = nextConfig
