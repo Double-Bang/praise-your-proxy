1ec6fd6944848b008beb0c35957367a494f7c4fd
molloy <yo@molloy.soy>
2023-07-12 10:39:40 -0500

    Update nextauth config
    Remove unnecessary credentials, add changes from these sources
    
    [0] https://next-auth.js.org/configuration/providers/oauth
    [1] https://next-auth.js.org/configuration/callbacks

--- next-auth.config.js
+++ next-auth.config.js
@@ -4,21 +4,31 @@ import Providers from "next-auth/providers";
 
 const options = {
   debug: true,
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        // Return false to display a default error message
+        return false;
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    }
+  },
   providers: [
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    }),
-    Providers.Credentials({
-      name: "Email and Password",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        // login logic coming soon
-      },
-    }),
+    }
   ],
   pages: {
     signIn: "/auth/signin",
